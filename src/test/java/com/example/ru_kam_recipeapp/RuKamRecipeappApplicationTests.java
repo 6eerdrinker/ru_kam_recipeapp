782b027b9c19269913be8b391b55bf3dd71e2402
doc <klempaleks@mail.ru>
2022-12-20 09:34:12 +0900

    First Commit

---
+++ src/test/java/com/example/ru_kam_recipeapp/RuKamRecipeappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.ru_kam_recipeapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RuKamRecipeappApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
