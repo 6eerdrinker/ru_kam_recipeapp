7e93b31f682cb1e081c5d33d8be3dc62b66b4e70
doc <klempaleks@mail.ru>
2022-12-27 12:39:56 +0900

    Homework API building done! Check please!!!

--- src/main/java/com/example/ru_kam_recipeapp/controller/IngredientController.java
+++ src/main/java/com/example/ru_kam_recipeapp/controller/IngredientController.java
@@ -4,7 +4,8 @@ import com.example.ru_kam_recipeapp.model.Ingredient;
 import com.example.ru_kam_recipeapp.service.IngredientService;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collection;
+import java.util.List;
+
 
 @RestController
 @RequestMapping("/ingredient")
@@ -16,18 +17,27 @@ public class IngredientController {
         this.ingredientService = ingredientService;
     }
 
-    @GetMapping
-    public Collection<Ingredient> getAllIngredients() {
-        return this.ingredientService.getAllIngredients();
+    @PostMapping
+    public Ingredient add(@RequestBody Ingredient ingredient) {
+        return ingredientService.add(ingredient);
     }
 
     @GetMapping("/{id}")
-    public Ingredient getIngredient(@PathVariable("id") String id) {
-        return this.ingredientService.getIngredient(id);
+    public Ingredient getIngredient(@PathVariable("id") long id) {
+        return ingredientService.get(id);
     }
 
-    @PostMapping
-    public Ingredient addIngredient(@RequestBody Ingredient ingredient) {
-        return this.ingredientService.addIngredient(ingredient);
+    @GetMapping
+    public List<Ingredient> getAll() {
+        return this.ingredientService.getAll();
+    }
+    @PutMapping("/{id}")
+    public Ingredient updateIngredient(@PathVariable("id") long id, @RequestBody Ingredient ingredient) {
+        return ingredientService.update(id, ingredient);
+    }
+
+    @DeleteMapping("/{id}")
+    public Ingredient deleteIngredient(@PathVariable("id") long id) {
+        return ingredientService.delete(id);
     }
 }
---
+++ src/main/java/com/example/ru_kam_recipeapp/record/InfoRecord.java
@@ -0,0 +1,38 @@
+package com.example.ru_kam_recipeapp.record;
+
+import java.time.LocalDate;
+
+public class InfoRecord {
+    private final String studentName;
+
+    private final String projectName;
+
+    private final LocalDate projectDate;
+
+    private final String description;
+
+    public InfoRecord(String studentName, String projectName, LocalDate projectDate, String description) {
+        this.studentName = studentName;
+        this.projectName = projectName;
+        this.projectDate = projectDate;
+        this.description = description;
+    }
+
+    public String getStudentName() {
+        return studentName;
+    }
+
+    public String getProjectName() {
+        return projectName;
+    }
+
+    public LocalDate getProjectDate() {
+        return projectDate;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+}
+
+
